b341479d8b986405babfa7b47a5e941397eff2e7
anshul-X-07 <anshulpatre@gmail.com>
2022-06-04 18:39:31 +0530

    updated with docker and NLP files

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """<h1 style="text-align: center;">Flask Demo App Working Fine</h1>"""
+    # return """<h1 style="text-align: center;color:red;font-size:80px;">Sentiment Analysis</h1>"""
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
